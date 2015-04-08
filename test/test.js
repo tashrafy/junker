5d1bbd3b2e5f56245c31b45e0c2e1b0b54b5404b
Taufiqur Ashrafy <tashrafy91@gmail.com>
2015-04-07 23:17:57 -0400

    finishing number() tests, beginning decimal

--- test/test.js
+++ test/test.js
@@ -21,25 +21,59 @@ describe('junker node module', function () {
   });
 
   it('must return a name obj', function(){
+    console.log('Junker.Name() TEST:\n');
     var nameObj = junker.Name()
       , name = nameObj.name;
 
-    console.log('junker.Name().name\n', nameObj);
+    console.log('name\n', nameObj);
     assert.equal('object', typeof nameObj, 'junker.Name() should return an obj');
     assert.equal('string', typeof name, 'name should be a string');
     assert.notStrictEqual(null, name, 'name is NULL');
     assert.notStrictEqual(undefined, name, 'name is undefined');
+
+    console.log('----------\n');
   });
 
-  it('must return a number obj', function(){
-    var number = junker.Number;
-
-      // console.log(number.decimal(4, 5));
+  var number = junker.Number;
+  it('must return a number', function(){
+    console.log('Junker.Number TEST:\n');
+    console.log('number', number);
     assert.equal('object', typeof number, 'junker.Number should return an object');
-    assert.equal(0, number.number(0), 'number(0) should return random number of digits');
-    // assert.notStrictEqual(null, name, 'name is NULL');
-    // assert.notStrictEqual(undefined, name, 'name is undefined');
+
+    var zeroCase = number.number(0);
+    console.log('number.number(0)', zeroCase);
+    assert.equal(true, zeroCase > 0, 'number(0) should return random number of digits');
+
+    var nanCase = number.number(NaN);
+    console.log('number.number(NaN)', nanCase);
+    assert.equal(true, nanCase > 0, 'number(NaN) should return random number of digits');
+
+    var emptyCase = number.number();
+    console.log('number.number()', emptyCase);
+    assert.equal(true, emptyCase > 0, 'number() should return random number of digits');
+
+    var strCase = number.number('123');
+    console.log('number.number("123")', strCase);
+    assert.equal(true, strCase > 0, 'number("123") should return random number of digits');
+
+    var wordCase = number.number('hello');
+    console.log('number.number("hello")', wordCase);
+    assert.equal(true, wordCase > 0, 'number("hello") should return random number of digits');
+
+    console.log('----------\n');
   });
+
+  it('must return a decimal', function(){
+    console.log('Junker.Number.decimal() TEST:\n');
+
+    var decimal = number.decimal(4, 5);
+    console.log('number.decimal(4, 5)', decimal);
+    assert.equal(false, parseFloat(decimal) % 1 === 0, 'decimal(4, 5) should return a decimal');
+
+    console.log('----------\n');
+  });
+
+
 });
 
 
